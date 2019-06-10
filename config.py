2d13e2e0429a08ecc14006c75c9e55836ae7a637
Nok Chan <channo@oocl.com>
2019-06-10 17:57:49 +0800

    [Feature] - Add logging config to config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
-HOME_PAGE = "https://hk.jobsdb.com/hk/jobs/information-technology/1"
+import logging
+LOG_FORMAT = "%(asctime)s | %(filename)s: %(lineno)d | %(funcName)s: | %(message)s"
+logging.basicConfig(
+            filename='training.log',
+            level=logging.DEBUG,
+            format=LOG_FORMAT)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# 設定輸出格式
+formatter = logging.Formatter(LOG_FORMAT)
+# handler 設定輸出格式
+console.setFormatter(formatter)
+# 加入 hander 到 root logger
+logging.getLogger('').addHandler(console)
---
+++ parser.py
@@ -0,0 +1,3 @@
+# import config
+from config import *
+logging.info("123")
